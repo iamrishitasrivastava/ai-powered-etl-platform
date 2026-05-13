d648b688863b0aecc8a1cd915b86a3993a3ea4b9
Rishita Srivastava <rishitaglenhill@gmail.com>
2026-05-13 13:57:28 +0530

    Added cleaned dataset comparison

--- app.py
+++ app.py
@@ -26,3 +26,35 @@ if uploaded_file:
     # Duplicate rows
     st.subheader("Duplicate Rows")
     st.write(df.duplicated().sum())
+
+    # =========================
+    # DATA CLEANING
+    # =========================
+
+    cleaned_df = df.drop_duplicates()
+
+    cleaned_df["salary"] = cleaned_df["salary"].fillna(0)
+
+    # CLEANED DATA
+    st.subheader("Cleaned Dataset")
+    st.dataframe(cleaned_df)
+
+    # CLEANED DATA INFO
+    st.subheader("Cleaned Dataset Information")
+
+    st.write("Rows:", cleaned_df.shape[0])
+    st.write("Columns:", cleaned_df.shape[1])
+        # =========================
+# BEFORE VS AFTER
+# =========================
+    
+    
+    st.subheader("Before vs After Cleaning")
+    
+    st.write("Original Rows:", df.shape[0])
+    
+    st.write("Cleaned Rows:", cleaned_df.shape[0])
+    
+    rows_removed = df.shape[0] - cleaned_df.shape[0]
+    
+    st.write("Rows Removed:", rows_removed)
